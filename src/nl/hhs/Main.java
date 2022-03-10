af9008486a08514ffe7cd19825cc174cc3f0ad91
codetariat <71725599+codetariat@users.noreply.github.com>
2022-03-10 13:29:57 +0100

    update

--- src/nl/hhs/Main.java
+++ src/nl/hhs/Main.java
@@ -1,8 +1,13 @@
 package nl.hhs;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println("gebruiker heeft dit ingevoerd: " + input);
     }
 }
