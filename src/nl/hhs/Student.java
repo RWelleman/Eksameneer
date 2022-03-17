db52518c3f35b283f51d08f0523534124b73b9ae
abd-el <el.abdullah@live.nl>
2022-03-17 13:57:04 +0100

    resultaten toegevoegd aan student.java

--- src/nl/hhs/Student.java
+++ src/nl/hhs/Student.java
@@ -1,8 +1,11 @@
 package nl.hhs;
 
+import java.util.ArrayList;
+
 public class Student {
     private int studentNummer;
     private String naam;
+    private ArrayList<Resultaat> resultaten;
 
     public Student(int studentNummer, String naam){
         this.studentNummer = studentNummer;
