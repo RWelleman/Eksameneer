0a8c3027fdfed76798b86f63566a88496e2eab50
el-abd <101092777+abd-el@users.noreply.github.com>
2022-03-21 15:07:24 +0100

    Antwoord en Vraag classes verder ontwikkeld

--- src/nl/hhs/Antwoord.java
+++ src/nl/hhs/Antwoord.java
@@ -1,5 +1,27 @@
 package nl.hhs;
 
 public class Antwoord {
+    private String inhoud;
+    private boolean isGoed;
 
+    public Antwoord(String inhoud, boolean isGoed) {
+        this.inhoud = inhoud;
+        this.isGoed = isGoed;
+    }
+
+    public String getInhoud() {
+        return inhoud;
+    }
+
+    public void setInhoud(String inhoud) {
+        this.inhoud = inhoud;
+    }
+
+    public boolean isGoed() {
+        return isGoed;
+    }
+
+    public void setGoed(boolean goed) {
+        isGoed = goed;
+    }
 }
