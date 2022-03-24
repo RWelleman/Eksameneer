3e0baa7bc68dcb42119256bf2d8fa1a26adf1678
Mike <contact.mikedeboer@gmail.com>
2022-03-24 13:56:50 +0100

    Made some changes

--- src/main/java/nl/hhs/Student.java
+++ src/main/java/nl/hhs/Student.java
@@ -5,10 +5,22 @@ import java.util.ArrayList;
 public class Student {
     private int studentNummer;
     private String naam;
-    private ArrayList<Resultaat> resultaten;
+    private ArrayList<Resultaat> behaaldeExamens;
 
     public Student(int studentNummer, String naam){
         this.studentNummer = studentNummer;
         this.naam = naam;
+    }
+
+    public int getStudentNummer() {
+        return studentNummer;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public ArrayList<Resultaat> getBehaaldeExamens() {
+        return behaaldeExamens;
     }
 }
