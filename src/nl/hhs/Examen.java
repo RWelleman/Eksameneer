1f62f3f82afd2f153a5a7085a24345226ad893bf
TimmyGuy <tim.ohlsen2@gmail.com>
2022-03-24 15:15:02 +0100

    Run through questions and print grade

--- src/nl/hhs/Examen.java
+++ src/nl/hhs/Examen.java
@@ -1,12 +1,13 @@
 package nl.hhs;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Examen {
     private String examenCode;
     private ArrayList<Vraag> vragen;
     private ArrayList<Resultaat> resultaten;
-    private static ArrayList<Examen> alleExamen = new ArrayList<>();
+    public static ArrayList<Examen> alleExamen = new ArrayList<>();
 
     public Examen(ArrayList<Vraag> vragen, String examenCode) {
         this.vragen = vragen;
@@ -23,9 +24,31 @@ public class Examen {
     }
 
     public Resultaat neemAf(Student student){
+        Scanner scanner = new Scanner(System.in);
 
-        Resultaat resultaat = new Resultaat(student, this, 5.0);
-        return resultaat;
+        for (Vraag vraag : vragen) {
+            System.out.println(vraag.getInhoud());
+            Antwoord antwoord = new Antwoord(scanner.next());
+            vraag.setAntwoord(antwoord);
+        }
+
+        double cijfer = 1;
+
+        System.out.println("Lever examen in? (j/n)");
+        String jn = scanner.next();
+        if(jn.equals("j")) {
+            int correct = 0;
+            for (Vraag vraag : vragen) {
+                Antwoord antwoord = vraag.controleer();
+                correct += antwoord.isGoed() ? 1 : 0;
+            }
+            cijfer = correct / vragen.size() * 9 + 1;
+            System.out.println(vragen.size());
+            System.out.println(correct);
+            System.out.println(cijfer);
+        }
+
+        return new Resultaat(student, this, cijfer);
     }
 }
 
