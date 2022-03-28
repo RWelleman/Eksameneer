881e184b936ece07924ded30c373506d5b4ca5e0
abd-el <101092777+abd-el@users.noreply.github.com>
2022-03-28 14:38:19 +0200

    Update Eksameneer.java

--- src/main/java/nl/hhs/eksameneer/Eksameneer.java
+++ src/main/java/nl/hhs/eksameneer/Eksameneer.java
@@ -19,36 +19,29 @@ public class Eksameneer {
         // write your code here //
         Scanner scanner = new Scanner(System.in);
 
+        toonMenu();
+        int gekozen = scanner.nextInt();
+        while(gekozen != 0){
+            switch(gekozen) {
+                case 1:
+                    ArrayList<Examen> alleExamen = Examen.getAlleExamen();
+                    System.out.println("== Selecteer een examen ==");
+                    for(int i = 0; i< alleExamen.size(); i++){
+                        System.out.println(i+1 + ") " + alleExamen.get(i).getExamenCode());
+                    }
+                    System.out.println("0) Terug naar menu");
+                    System.out.println("====");
 
-        while (keuze != 0) {
-            System.out.println("Menu");
-            System.out.println("(1) Lijst met examens.");
-            System.out.println("(2) Lijst met studenten.");
-            System.out.println("(3) Nieuwe student inschrijven.");
-            System.out.println("(4) Student verwijderen.");
-            System.out.println("(5) Examen afnemen.");
-            System.out.println("(6) Is student geslaagd voor test?");
-            System.out.println("(7) Welke examens heeft student gehaald?");
-            System.out.println("(8) Welke student heeft de meeste examens gehaald?");
-            System.out.println("(0) Exit.");
-            System.out.print("Uw keuze:");
+                    int examenOptie = scanner.nextInt();
+                    if(examenOptie == 0){
+                        break;
+                    }
 
-            keuze = scanner.nextInt();
-
-            switch (keuze) {
-                case 1 -> System.out.println("nummer 1 is gekozen");
-                case 2 -> System.out.println("nummer 2 is gekozen");
-                case 3 -> System.out.println("nummer 3 is gekozen");
-                case 4 -> System.out.println("nummer 4 is gekozen");
-                case 5 -> System.out.println("nummer 5 is gekozen");
-                case 6 -> System.out.println("nummer 6 is gekozen");
-                case 7 -> System.out.println("nummer 7 is gekozen");
-                case 8 -> System.out.println("nummer 8 is gekozen");
-                case 0 -> {
-                    System.out.println("exit");
-                    System.exit(0);
-                }
+                    break;
             }
-        }
+            toonMenu();
+            gekozen = scanner.nextInt();
+        };
+        //Examen nieuweExamen = new Examen();
     }
 }
