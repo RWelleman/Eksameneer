4983993f83ba239e18d05df0d0a42335990fe12f
Tim Ohlsen <tim.ohlsen2@gmail.com>
2022-04-11 14:25:00 +0200

    Updated junit test method names based on conventions

--- src/test/java/nl/hhs/eksameneer/examen/ExamenTest.java
+++ src/test/java/nl/hhs/eksameneer/examen/ExamenTest.java
@@ -2,6 +2,7 @@ package nl.hhs.eksameneer.examen;
 
 import nl.hhs.eksameneer.vraag.GeslotenVraag;
 import nl.hhs.eksameneer.vraag.Vraag;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,9 +11,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ExamenTest {
     @Test
-    void newExamenWithNullParametersShouldntBeAddedToAllExams() {
+    @DisplayName("Zonder parameters geen examen toevoegen")
+    void examen_creatingNewExamenWithNullParameters_examenShouldntBeAddedToAlleExamens() {
         Examen examenFullyEmpty = new Examen(null, null);
-        Examen examenNoQuestions = new Examen(new ArrayList<Vraag>(), null);
+        Examen examenNoQuestions = new Examen(new ArrayList<>(), null);
         Examen examenNoCode = new Examen(null, "Test examencode");
         ArrayList<Examen> alleExamens = Examen.alleExamen;
 
@@ -22,7 +24,8 @@ public class ExamenTest {
     }
 
     @Test
-    void neemAfShouldGetNull() {
+    @DisplayName("Zonder student geen examen afnemen")
+    void neemAf_withoutaStudentAsParameterInFunction_shouldReturnNull() {
         ArrayList<Vraag> vragen = new ArrayList<>();
         vragen.add(new GeslotenVraag("Deze test slaagt. Ja of Nee", "Ja"));
         Examen examen = new Examen(vragen, "SlaagtTest");
@@ -31,7 +34,8 @@ public class ExamenTest {
     }
 
     @Test
-    void returnAnArrayListOfAllVragen() {
+    @DisplayName("Geef alle vragen terug van een examen")
+    void getVragen_questionsAreAddedToTheExamen_shouldReturnAnArrayListOfVragen() {
         ArrayList<Vraag> vragen = new ArrayList<>();
         vragen.add(new GeslotenVraag("Deze test slaagt. Ja of Nee", "Ja"));
         Examen examen = new Examen(vragen, "SlaagtTest");
